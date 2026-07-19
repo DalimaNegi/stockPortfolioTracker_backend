6f7b7333f68b9c7f23821b8f85877a3639b17ce5
Dalima Negi <dalimanegi845@gmail.com>
2026-07-19 13:43:40 +0530

    change for cookie

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -31,7 +31,7 @@ const login = async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict',
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 Days
     });
 
